4e81ba4b5ead0469774de339442a9cb431778b5a
jsfmj <68734739+jsfmj@users.noreply.github.com>
2021-05-17 20:27:15 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function purple(){
+document.body.style.background="plum"
+}
+
+function green(){
+    document.body.style.background="greenyellow"
+}
+
+function yellow(){
+    document.body.style.background="yellow"
+}
